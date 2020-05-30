54e382047e09408a8f202b0b6924e4c2744b0e72
ClementDidom <clement@freshplanet.com>
2020-05-30 02:58:19 -0400

    fix: dependancies added : Dashboard view

---
+++ src/views/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <p>Welcome to the MSSandbox Admin!</p>
+      <p>The work is under progress, but the release will come asap!</p>
+    </Container>
+  );
+};
